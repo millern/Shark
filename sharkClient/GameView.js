60873fdb5211cdc6d686ebe5945757ffa104bf42
Hack Reactor Students <github@hackreactor.com>
2013-05-27 10:39:51 -0700

    refactored to use handlebars

--- sharkClient/GameView.js
+++ sharkClient/GameView.js
@@ -30,76 +30,45 @@ var GameView = Backbone.View.extend({
   },
 
   template: function(){
-    Handlebars.registerHelper('guessWord1', function(game){
-      var trail = game.guessing === game.player2 || game.winner ? ' disabled />' : ' />';
+    Handlebars.registerHelper('guessWord', function(game, player){
+      var trail = game.guessing === player || game.winner ? ' disabled />' : ' />';
       return new Handlebars.SafeString('<input class="guess" placeholder="guess word"' + trail);
     });
-    Handlebars.registerHelper('setWord1', function(game){
-      var trail = !! game.word1 ? ' disabled />' : ' />';
-      return new Handlebars.SafeString('<input class="set" placeholder="set word"' + trail);
-    });
-    Handlebars.registerHelper('guessWord2', function(game){
-      var trail = game.guessing === game.player1 || game.winner ? ' disabled />' : ' />';
-      return new Handlebars.SafeString('<input class="guess" placeholder="guess word"' + trail);
-    });
-    Handlebars.registerHelper('setWord2', function(game){
-      var trail = !! game.word2 ? ' disabled />' : ' />';
+    Handlebars.registerHelper('setWord', function(word){
+      var trail = !! word ? ' disabled />' : ' />';
       return new Handlebars.SafeString('<input class="set" placeholder="set word"' + trail);
     });
     Handlebars.registerHelper('gameState', function(game){
       return game.winner? 'winner: ' + game.winner : game.guessing + ' is guessing';
     });
+    Handlebars.registerHelper('guesses', function(guesses){
+      return new Handlebars.SafeString(new GuessesView({collection: guesses}).render()[0].outerHTML);
+    });
     return Handlebars.compile(
       '<div class="row">'+
-      '<div class="gameState span2 offset4">{{gameState this}}</div>' +
+        '<div class="gameState span2 offset4">{{gameState this}}</div>' +
       '</div>' +
       '<div class="row">' +
-      '<div class="player1Side span4 offset1">' +
-      '{{setWord1 this}}'+
-      '<div>{{player1}}</div>'+
-      '{{guessWord1 this}}'+
-      '<div>{{word1}}</div>'+
-      '<ul>{{#each word1Guesses}}<li>{{this.guess}} {{this.score}}</li>{{/each}}</ul>'+
-      '</div>'+
-      '<div class="player2Side span4 offset1">' +
-      '{{setWord2 this}}'+
-      '<div>{{player2}}</div>'+
-      '{{guessWord2 this}}'+
-      '<div>{{word2}}</div>'+
-      '<ul>{{#each word1Guesses}}<li>{{this.guess}} {{this.score}}</li>{{/each}}</ul>'+
-      '</div>'+
+        '<div class="player1Side span4 offset1">' +
+          '{{setWord this.word1}}'+
+          '<div>{{player1}}</div>'+
+          '{{guessWord this this.player1}}'+
+          '<div>{{word1}}</div>'+
+          '{{guesses this.word1Guesses}}'+
+        '</div>'+
+        '<div class="player2Side span4 offset1">' +
+          '{{setWord this.word2}}'+
+          '<div>{{player2}}</div>'+
+          '{{guessWord this this.player2}}'+
+          '<div>{{word2}}</div>'+
+          '{{guesses this.word2Guesses}}'+
+        '</div>'+
       '</div>');
   },
 
   render: function(){
     this.$el.children().detach();
     var tmplt = this.template();
-    // $gameState = $('<div class="row"></div>');
-    // $('<div class="gameState span2 offset4"></div>')
-      // .text(this.model.get('winner') ?
-        // 'winner: ' + this.model.get('winner') :
-        // this.model.get('guessing')+ " is guessing"
-        // ).appendTo($gameState);
-
-    // $gamePlay = $('<div class="row"></div>');
-    // $player1Side = $('<div class="player1Side span4 offset1"></div>').append(
-    //   $('<input class="set" placeholder="set word"/>')
-    //     .prop("disabled",!!this.model.get("word1")),
-    //   $('<div></div>').text(this.model.get('player1')),
-    //   $('<input class="guess" placeholder="guess word"/>')
-    //     .prop("disabled",this.model.get("guessing") === this.model.get("player2") || this.model.get("winner") ? true : false),
-    //   $('<div></div>').text(this.model.get('word1')),
-    //   new GuessesView({collection: this.model.get('word1Guesses')}).render()
-      // ).appendTo($gamePlay);
-    // $player2Side = $('<div class="player2Side span4"></div>').append(
-    //   $('<input class="set" placeholder="set word"/>')
-    //     .prop("disabled",!!this.model.get("word2")),
-    //   $('<div></div>').text(this.model.get('player2')),
-    //   $('<input class="guess" placeholder="guess word"/>')
-    //     .prop("disabled",this.model.get("guessing") === this.model.get("player1") || this.model.get("winner") ? true : false),
-    //   $('<div></div>').text(this.model.get('word2')),
-    //   new GuessesView({collection: this.model.get('word2Guesses')}).render()
-    //   ).appendTo($gamePlay);
     return this.$el.html(tmplt(this.model.toJSON()));
   }
 });
