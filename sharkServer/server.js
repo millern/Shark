59bb30128b08cf98bdc1d09f1d9fd1932726fd8d
Nick Miller <njmiller6@gmail.com>
2013-05-27 10:04:49 -0700

    Added variables for ip and port

--- sharkServer/server.js
+++ sharkServer/server.js
@@ -8,9 +8,11 @@ var express = require('../lib/node_modules/express'),
 
 app.use('/lib', express.static(__dirname + '/../lib'));
 app.use('/', express.static(__dirname + '/../sharkClient'));
+var ip = '10.0.1.82';
+var port = 8080;
 
-server.listen(8080, '192.168.1.15');
-console.log('listening on 192.168.1.15:8080');
+server.listen(port, ip);
+console.log('listening on 192.168.1.4:8080');
 
 var socketHandler = require('./socketHandler')(io);
 io.sockets.on('connection', socketHandler.handler);
