64a78ff72817826d2f4ed51b83a197d79ef6f599
Hack Reactor Students <github@hackreactor.com>
2013-05-27 10:40:23 -0700

    Log message uses variables

--- sharkServer/server.js
+++ sharkServer/server.js
@@ -8,11 +8,11 @@ var express = require('../lib/node_modules/express'),
 
 app.use('/lib', express.static(__dirname + '/../lib'));
 app.use('/', express.static(__dirname + '/../sharkClient'));
-var ip = '10.0.1.82';
+var ip = '10.0.1.23';
 var port = 8080;
 
 server.listen(port, ip);
-console.log('listening on 192.168.1.4:8080');
+console.log('listening on ' + ip + ':' + port);
 
 var socketHandler = require('./socketHandler')(io);
 io.sockets.on('connection', socketHandler.handler);
