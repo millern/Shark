91b35a52da72fa9e8711fb918993d78a9b0070e4
Hack Reactor Students <github@hackreactor.com>
2013-05-29 17:34:13 -0700

    cleaned up lib, packages, and gitignore

--- sharkServer/server.js
+++ sharkServer/server.js
@@ -11,6 +11,10 @@ app.use('/', express.static(__dirname + '/../sharkClient'));
 var ip = '10.0.1.23';
 var port = 8080;
 
+process.argv.forEach(function(val,index,array){
+  console.log(index + ": " + val);
+});
+
 server.listen(port, ip);
 console.log('listening on ' + ip + ':' + port);
 
